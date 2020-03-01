c074c53690de64aa6fcbbb86745bfaaa3e24f7ab
Ovy95 <jackoverton21@hotmail.com>
2020-03-01 17:45:49 +0000

    Can roll a space

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -6,23 +6,38 @@ describe('Bowling Game', function () {
       game = new BowlingGame();
     });
 
-    it('Can create a new game', function() {
-      var game = new BowlingGame();
+
+    it('can score one pin', function() {
+      game.roll(1);
+      frame(0, 20);
+      expect(game.score()).toBe(1);
+
     });
 
-  it('can roll gutter game', function () {
-    var game = new BowlingGame();
-    for (var i = 0; i < 20; i++) {
-      game.roll(0);
-    }
-    expect(game.score()).toBe(0);
-  });
+    it('can roll gutter game', function () {
+        frame(0, 20);
+        expect(game.score()).toBe(0);
+    });
 
-  it('can roll all ones', function(){
-    for (var i = 0; i < 20; i++) {
-      game.roll(1)
-    }
-    expect(game.score()).toBe(20);
-  });
+    it('can roll all ones', function() {
+      frame(1, 20);
+      expect(game.score()).toBe(20);
+    });
+
+    it('Can roll a spare', function(){
+      game.roll(5);
+      game.roll(5);
+      game.roll(3);
+      frame(0, 17);
+      expect(game.score()).toBe(16);
+    })
+
+
+
+    var frame = function (pins, rolls) {
+      for (var i = 0; i < rolls; i++) {
+        game.roll(pins);
+      }
+  };
 
 });
